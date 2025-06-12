72f92189fd1c42823af1501b608b68e9ef7cfcac
sheshanand <iamsheshanandjha@gmail.com>
2025-06-13 00:05:56 +0100

    changes to include city list with number

--- src/App.js
+++ src/App.js
@@ -11,18 +11,22 @@ function App() {
   const[letter, setLetter] = useState('');
   const [count, setCount] = useState('');
   const [error, setError] = useState('');
+  const [cities, setCities] = useState([])
 
   const getCity =  async (e) => {
       e.preventDefault();
       setError('');
       setCount(null);
 
+
     if (!letter.match(/^[a-zA-Z]$/)) {
       setError('Please enter a letter');
     }
     try {
         const response = await axiosInstance.get(`/cities/count?letter=${letter}`);
-        setCount(response.data)
+        const {count, cities} = response.data;
+        setCount(count)
+        setCities(cities || [])
     }catch (err){
         setError(err.message+' Please check the url '+baseUrl);
     }
@@ -37,7 +41,8 @@ function App() {
             </label>
           <button type="submit">Get City Count</button>
         </form>
-          {<p>Number of cities starting with letter "{letter}": {count}</p>}
+          {count && <p>Number of cities starting with letter "{letter}": {count}</p>}
+          {cities.length>0 && <p>Cities : {cities.join(", ")}</p>}
           {error && <p className="error">{error}</p>}
       </div>
   )
